fc7e33303c6fb7d395bf1c3d59481eb9d855aae4
ItaiNatan <itai08425@gmail.com>
2020-01-26 22:12:49 +0200

    more updates

--- server.js
+++ server.js
@@ -13,13 +13,6 @@ app.use("/api/start", start);
 const end = require("./routes/api/end");
 app.use("/api/end", end);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
-
 const PORT = process.env.PORT || 5000;
 
 var server = http.listen(PORT, () => {
